7ed2391179c236504ba43b213dd877f0bd8283fa
David Calderon <davidjcr200040@gmail.com>
2021-12-05 20:03:07 -0400

    header component

---
+++ src/components/ui/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../../src/index.css';
+
+export const Header = () => {
+    return (
+        <div class="header-title">
+            <h4 id="title-1"><b>Github</b></h4>
+            <h4 id="title-2">Jobs</h4>
+        </div>
+    )
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Header } from './components/ui/Header.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
